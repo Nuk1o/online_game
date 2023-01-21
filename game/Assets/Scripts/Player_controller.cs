86d9d4ab9286a1d3fb23be7657d55ca7fc55fc07
Nukio <NukiXm@yandex.ru>
2023-01-22 01:58:33 +0300

    Create menu and btn connection

--- game/Assets/Scripts/Player_controller.cs
+++ game/Assets/Scripts/Player_controller.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Mirror;
 
-public class Player_controller : MonoBehaviour
+public class Player_controller : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+    private Vector2 input;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isLocalPlayer) return;
+
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Flip();
+        CameraMov();
+    }
+
+    private void CameraMov()
+    {
+        Camera.main.transform.localPosition = new Vector3(transform.position.x, transform.position.y, -1f);
+        transform.position =
+            Vector2.MoveTowards(transform.position, Camera.main.transform.localPosition, Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (Input.GetAxis("Horizontal")>0)
+        {
+            transform.localRotation = Quaternion.Euler(0,180,0);
+        }
+
+        if (Input.GetAxis("Horizontal")<0)
+        {
+            transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * speed / 100);
     }
 }
